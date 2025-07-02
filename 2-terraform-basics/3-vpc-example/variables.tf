0544d4a552d1e62e63ecfa7231c3f4819a03c017
RamMaths <ramses.hdz30@gmail.com>
2025-07-02 11:31:32 -0600

    Add hashicorp configuration language to the terrfaorm basics chapter

--- 2-terraform-basics/3-vpc-example/variables.tf
+++ 2-terraform-basics/3-vpc-example/variables.tf
@@ -1,15 +1,15 @@
 variable "aws_region" {
-  type = string
+  type    = string
   default = "us-east-2"
 }
 
 variable "vpc_name" {
-  type = string
+  type    = string
   default = "demo_vpc"
 }
 
 variable "vpc_cidr" {
-  type = string
+  type    = string
   default = "10.0.0.0/16"
 }
 
